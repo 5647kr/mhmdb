82ed1c57090531b6c2bccd2fe0016f44ac0cd621
5647kr <119591236+5647kr@users.noreply.github.com>
2024-09-20 16:03:12 +0900

    [✨ Feat] MainPage searchWrap 기능 추가

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const header = document.querySelector("header");
 const main = document.querySelector("main")
 const menuBtn = header.querySelector(".menuBtn");
+const searchBtn = header.querySelector(".searchBtn");
 
 menuBtn.addEventListener("click", () => {
   const aside = main.querySelector("aside");
@@ -9,4 +10,13 @@ menuBtn.addEventListener("click", () => {
 
   aside.classList.toggle("active");
   isActive ? menuBtnImg.src="../img/common/메뉴창.svg" : menuBtnImg.src="../img/common/되돌아가기.svg"
+});
+
+searchBtn.addEventListener("click", () => {
+  const search = header.querySelector(".searchWrap");
+  const isActive = search.classList.contains("active");
+  const searchBtnImg = searchBtn.querySelector("img");
+
+  search.classList.toggle("active");
+  isActive ? searchBtnImg.src="../img/common/검색.svg" : searchBtnImg.src="../img/common/닫기.svg"
 })
