cff3c87f254c425fc5dd5af2e16c9c58481b5dd0
5647kr <119591236+5647kr@users.noreply.github.com>
2024-09-21 23:22:55 +0900

    [🔨 Refactor] MainPage 버튼 이벤트 함수 수정

--- js/main.js
+++ js/main.js
@@ -1,22 +1,45 @@
-const header = document.querySelector("header");
-const main = document.querySelector("main")
-const menuBtn = header.querySelector(".menuBtn");
-const searchBtn = header.querySelector(".searchBtn");
+function ActiveBtn () {
+  const header = document.querySelector("header");
+  const main = document.querySelector("main")
+  const menuBtn = header.querySelector(".menuBtn");
+  const searchBtn = header.querySelector(".searchBtn");
+  
+  menuBtn.addEventListener("click", () => {
+    const aside = main.querySelector(".asideWrap");
+    const isActive = aside.classList.contains("active");
+    const menuBtnImg = menuBtn.querySelector("img");
 
-menuBtn.addEventListener("click", () => {
-  const aside = main.querySelector(".asideWrap");
-  const isActive = aside.classList.contains("active");
-  const menuBtnImg = menuBtn.querySelector("img");
+    aside.classList.toggle("active");
 
-  aside.classList.toggle("active");
-  isActive ? menuBtnImg.src="../img/common/메뉴창.svg" : menuBtnImg.src="../img/common/되돌아가기.svg"
-});
+    if (isActive) {
+      menuBtnImg.src = "../img/common/메뉴창.svg";
+      menuBtnImg.alt = "메뉴 버튼";
+      searchBtn.disabled = false;
+    } else {
+      menuBtnImg.src = "../img/common/되돌아가기.svg";
+      menuBtnImg.alt = "되돌아가기 버튼";
+      searchBtn.disabled = true;
+      search.classList.remove("active");
+    }
+  });
 
-searchBtn.addEventListener("click", () => {
-  const search = header.querySelector(".searchWrap");
-  const isActive = search.classList.contains("active");
-  const searchBtnImg = searchBtn.querySelector("img");
+  searchBtn.addEventListener("click", () => {
+    const search = header.querySelector(".searchWrap");
+    const isActive = search.classList.contains("active");
+    const searchBtnImg = searchBtn.querySelector("img");
 
-  search.classList.toggle("active");
-  isActive ? searchBtnImg.src="../img/common/검색.svg" : searchBtnImg.src="../img/common/닫기.svg"
-})
+    search.classList.toggle("active");
+
+    if (isActive) {
+      searchBtnImg.src = "../img/common/검색.svg";
+      searchBtnImg.alt = "검색 버튼";
+      menuBtn.disabled = false;
+    } else {
+      searchBtnImg.src = "../img/common/닫기.svg";
+      searchBtnImg.alt = "닫기 버튼";
+      menuBtn.disabled = true;
+      aside.classList.remove("active");
+    }
+  });
+}
+ActiveBtn();
