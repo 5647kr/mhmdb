d8802653d58c52324c3de6de966b53d7915e239a
5647kr <119591236+5647kr@users.noreply.github.com>
2024-09-27 14:42:22 +0900

    [🔨 Refactor] MainPage 카드 UI 작업 및 Class화

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
-class MainPageEvent {
+class MainPage {
   constructor () {
-    this.cardList = document.querySelector(".cardWrap ul");
-    const aside = document.querySelector("aside");
-    this.typeList = aside.querySelector(".typeList");
-    this.seriesList = aside.querySelector(".seriesList");
+    const header = document.querySelector("header");
+    this.menuBtn = header.querySelector(".menuBtn");
+    this.searchBtn = header.querySelector(".searchBtn");
+    this.searchWrap = header.querySelector(".searchWrap");
+
+    const main = document.querySelector("main");
+    this.aside = main.querySelector("aside");
+    this.typeList = main.querySelector(".typeList");
+    this.seriesList = main.querySelector(".seriesList");
+    this.cardList = main.querySelector(".cardWrap ul");
   }
 
   // 데이터 받는 함수
   async dataSet(data) {
     this.monsterData(data[0].monsterList);
-    this.typeData(data[1].typeList);
-    this.seriesData(data[2].seriesList);
+    // this.typeData(data[1].typeList);
+    // this.seriesData(data[2].seriesList);
   }
 
   // 카드 생성 함수
@@ -19,10 +25,10 @@ class MainPageEvent {
     data.map((monster) => {
       const monsterItem = document.createElement("li");
       const monsterCard = `
-        <a href="#" data-name="${monster.name}" data-type="${monster.type}" data-img="${monster.img}" data-weak="${monster.weak}" data-element="${monster.element}" data-alignment="${monster.alignment}">
+        <a href="/monster?='${monster.name}'" data-name="${monster.name}" data-type="${monster.type}" data-img="${monster.img}" data-weak="${monster.weak}" data-element="${monster.element}" data-alignment="${monster.alignment}">
           <article>
             <p>${monster.type}</p>
-            <img src="${monster.img}" alt="${monster.name}">
+            <img src="${monster.icon}" alt="${monster.name}">
             <h3>${monster.name}</h3>
             <p class="a11y-hidden">${monster.seriesId}</p>
           </article>
@@ -33,10 +39,52 @@ class MainPageEvent {
       docFrag.append(monsterItem);
     })
     this.cardList.append(docFrag);
+
+  }
+
+  // 모바일 태블릿 버튼 기능
+  MainPageEvent () {
+    // 버튼 클릭시 메뉴창 & 검색창 여닫기 기능
+    this.menuBtn.addEventListener("click", () => {
+      const isActive = this.aside.classList.contains("active");
+      const menuBtnImg = this.menuBtn.querySelector("img");
+
+      this.aside.classList.toggle("active");
+
+      if (isActive) {
+        menuBtnImg.src = "./img/common/menu.svg";
+        menuBtnImg.alt = "메뉴 버튼";
+        this.searchBtn.disabled = false;
+      } else {
+        menuBtnImg.src = "./img/common/back.svg";
+        menuBtnImg.alt = "되돌아가기 버튼";
+        this.searchBtn.disabled = true;
+        this.searchWrap.classList.remove("active");
+      }
+    });
+
+    this.searchBtn.addEventListener("click", () => {
+      const isActive = this.searchWrap.classList.contains("active");
+      const searchBtnImg = this.searchBtn.querySelector("img");
+
+      this.searchWrap.classList.toggle("active");
+
+      if (isActive) {
+        searchBtnImg.src = "../img/common/search.svg";
+        searchBtnImg.alt = "검색 버튼";
+        this.menuBtn.disabled = false;
+      } else {
+        searchBtnImg.src = "../img/common/close.svg";
+        searchBtnImg.alt = "닫기 버튼";
+        this.menuBtn.disabled = true;
+        this.aside.classList.remove("active");
+      }
+    });
+
   }
 }
 
-export default MainPageEvent;
+export default MainPage;
 
 
 
@@ -49,68 +97,7 @@ export default MainPageEvent;
 
 
 
+// // 메뉴 버튼 클릭 이벤트
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // 카드 생성 함수
-//   function ActiveBtn {
-//     const header = document.querySelector("header");
-//     const main = document.querySelector("main")
-//     const menuBtn = header.querySelector(".menuBtn");
-//     const searchBtn = header.querySelector(".searchBtn");
-
-//     menuBtn.addEventListener("click", () => {
-//       const aside = main.querySelector(".asideWrap");
-//       const isActive = aside.classList.contains("active");
-//       const menuBtnImg = menuBtn.querySelector("img");
-
-//       aside.classList.toggle("active");
-
-//       if (isActive) {
-//         menuBtnImg.src = "./img/common/menu.svg";
-//         menuBtnImg.alt = "메뉴 버튼";
-//         searchBtn.disabled = false;
-//       } else {
-//         menuBtnImg.src = "./img/common/back.svg";
-//         menuBtnImg.alt = "되돌아가기 버튼";
-//         searchBtn.disabled = true;
-//         search.classList.remove("active");
-//       }
-//     });
-
-//     searchBtn.addEventListener("click", () => {
-//       const search = header.querySelector(".searchWrap");
-//       const isActive = search.classList.contains("active");
-//       const searchBtnImg = searchBtn.querySelector("img");
-
-//       search.classList.toggle("active");
-
-//       if (isActive) {
-//         searchBtnImg.src = "../img/common/search.svg";
-//         searchBtnImg.alt = "검색 버튼";
-//         menuBtn.disabled = false;
-//       } else {
-//         searchBtnImg.src = "../img/common/close.svg";
-//         searchBtnImg.alt = "닫기 버튼";
-//         menuBtn.disabled = true;
-//         aside.classList.remove("active");
-//       }
-//     });
-//   }
-// // ActiveBtn();
+// // 검색 버튼 클릭 이벤트
